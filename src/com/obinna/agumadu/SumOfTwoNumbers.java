4f388d68b4a1a342633cd876cc2ae15979e185eb
oca070916 <oca010117@gmail.com>
2017-11-23 03:55:53 -0600

    Sum of 2 numbers -- trivial example

---
+++ src/com/obinna/agumadu/SumOfTwoNumbers.java
@@ -0,0 +1,15 @@
+package com.obinna.agumadu;
+
+public class SumOfTwoNumbers {
+	
+	public static void main(String[] args) {
+		
+		int number1 = 74;
+		int number2 = 36;
+		
+		System.out.println("Sum of " + number1 + " AND " + number2 
+				+ " is: " + (number1 + number2));
+		
+	}
+
+}
