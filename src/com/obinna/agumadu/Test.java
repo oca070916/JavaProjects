68f588707111af19cf0f235509576de9e813c40d
oca070916 <oca010117@gmail.com>
2017-11-23 04:48:42 -0600

    Testing github message on eclipse

---
+++ src/com/obinna/agumadu/Test.java
@@ -0,0 +1,12 @@
+package com.obinna.agumadu;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Trying to figure out this message:"
+				+ " non-fast-forward");
+		
+	}
+
+}
