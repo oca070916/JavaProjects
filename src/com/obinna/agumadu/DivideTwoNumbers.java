06f6be97ffb15bdf310d4a6746a583e3f7b1d418
oca070916 <oca010117@gmail.com>
2017-11-23 04:03:50 -0600

    Current program --> DivideTwoNumbers.java

---
+++ src/com/obinna/agumadu/DivideTwoNumbers.java
@@ -0,0 +1,17 @@
+package com.obinna.agumadu;
+
+public class DivideTwoNumbers {
+	
+	public static void main(String[] args) {
+		
+		float number1 = 176.0f;
+		float number2 = 45.0f;
+		
+		System.out.println(number1 + " divided by " + number2 + " = "
+				+ number1/number2);
+		
+		// prints -->  176.0 divided by 45.0 = 3.911111
+		
+	}
+
+}
