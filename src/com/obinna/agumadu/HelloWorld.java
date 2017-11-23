718bc6bff0dddd22d460a1dcc09b41d60268e7b1
oca070916 <oca010117@gmail.com>
2017-11-23 03:40:02 -0600

    Best way to prove one is capable is to show what one is capable of.

---
+++ src/com/obinna/agumadu/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.obinna.agumadu;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("Seems like the obligatory way to "
+				+ "begin any programming endeavor these days!");
+	
+	}
+}
