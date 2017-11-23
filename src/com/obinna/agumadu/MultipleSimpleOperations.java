877251e6a78de15a64cf2e6a8fed798d134ceba7
oca070916 <oca010117@gmail.com>
2017-11-23 04:13:22 -0600

    Current is MultipleSimpleOperations.java

---
+++ src/com/obinna/agumadu/MultipleSimpleOperations.java
@@ -0,0 +1,27 @@
+package com.obinna.agumadu;
+
+public class MultipleSimpleOperations {
+	
+	public static void main(String[] args) {
+		
+		//Produce the following outputs:
+		/*
+		 	a. -5 + 8 * 6
+			b. (55+9) % 9 
+			c. 20 + -3*5 / 8 
+			d. 5 + 15 / 3 * 2 - 8 % 3 
+		 */
+		
+		System.out.println(" -5 + 8 * 6 =               " + (-5 + 8 * 6));
+		System.out.println("(55+9) % 9 =                " + ((55 + 9) % 9));
+		System.out.println("20 + -3*5 / 8 =             " + (20 + -3 * 5 / 8));
+		System.out.println("5 + 15 / 3 * 2 - 8 % 3 =    " + (5 + 15 / 3 * 2 - 8 % 3));
+		
+		// 43
+		// 1
+		// 19
+		// 13
+		
+	}
+
+}
