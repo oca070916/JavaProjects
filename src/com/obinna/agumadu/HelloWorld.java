70f69454407788513c6b977018ce234a197af691
oca070916 <oca010117@gmail.com>
2017-11-23 05:29:19 -0600

    Lost 2

--- src/com/obinna/agumadu/HelloWorld.java
+++ src/com/obinna/agumadu/HelloWorld.java
@@ -5,8 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
+		System.out.println("\n");
 		System.out.println("Seems like the obligatory way to "
 				+ "begin any programming endeavor these days!");
+		
 	
 	}
 }
