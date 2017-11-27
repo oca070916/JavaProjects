7126263715be5483593c4dbb44906507debb9b3c
oca070916 <oca010117@gmail.com>
2017-11-27 16:52:24 -0600

    OOP 1

---
+++ Dog_Class/Dog.java
@@ -0,0 +1,38 @@
+
+public class Dog {
+	
+	private int size;
+	private String bread;
+	private String name;
+	
+	public void bark() {
+		
+		System.out.println("Ruff! Ruff!");
+		
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public String getBread() {
+		return bread;
+	}
+
+	public void setBread(String bread) {
+		this.bread = bread;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ Dog_Class/DogTestDrive.java
@@ -0,0 +1,16 @@
+
+public class DogTestDrive {
+	
+	public static void main(String[] args) {
+		
+		Dog fido = new Dog();
+		
+		fido.setSize(40);
+		
+		System.out.println("The dog's size is " + fido.getSize());
+		fido.bark();
+		
+		
+	}
+
+}
